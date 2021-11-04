3c76eeef7e1e79c238efcda15b2cfb5f23a1e248
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2021-11-04 21:44:40 +0100

    Change resources

--- src/components/pages/ComingSoonPage.jsx
+++ src/components/pages/ComingSoonPage.jsx
@@ -2,17 +2,21 @@ import React, {useState} from 'react';
 import '../../styles/comingSoonPage.scss';
 import ImageContainer from '../ui/ImageContainer';
 import SignupFormContainer from '../ui/SignupFormContainer';
-import boardGameImg from '../../resources/img_box.svg';
+import boardGameImg from '../../resources/img_box.png';
+import jumper1Img from '../../resources/img_jumper1.png';
 
 const ComingSoonPage = () => {
 
 
     return (
         <div className="coming-soon-page">
-            <div className="content-center">
+            <div className="box-img-container">
                 <ImageContainer imgPath={boardGameImg} alt="404"/>
             </div>
-            <div className="content-center">
+            <div className="jumper-img-container">
+                <ImageContainer imgPath={jumper1Img} alt="404"/>
+            </div>
+            <div className="signup-form-container">
                 <SignupFormContainer />
             </div>
         </div>
