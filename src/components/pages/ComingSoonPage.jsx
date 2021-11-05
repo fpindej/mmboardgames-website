e46091382730bf796e505f81c6e357eb383c7e49
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2021-11-05 23:05:24 +0100

    Add more jumpers, randomize their selection, improve styling

--- src/components/pages/ComingSoonPage.jsx
+++ src/components/pages/ComingSoonPage.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../../styles/comingSoonPage.scss';
 import ImageContainer from '../ui/ImageContainer';
 import GameLogoContainer from '../ui/GameLogoContainer';
 import SignupFormContainer from '../ui/SignupFormContainer';
 import boardGameImg from '../../resources/img_boxWithFigure.png';
 import jumper1Img from '../../resources/img_jumper1.png';
+import jumper2Img from '../../resources/img_jumper2.png';
+import jumper3Img from '../../resources/img_jumper3.png';
+import jumper4Img from '../../resources/img_jumper4.png';
+
 import gameTitleImg from '../../resources/img_gameTitle.png'
 
 const ComingSoonPage = () => {
+    const images= [
+        jumper1Img,
+        jumper2Img,
+        jumper3Img,
+        jumper4Img,
+        ];
+    
+    const [imgIndex, setImgIndex] = useState(0);
 
+    useEffect(() => {
+        let randomIndex = Math.floor(Math.random() * images.length);
+        setImgIndex(randomIndex);
+        }, [images.length]);
 
     return (
         <div className="coming-soon-page">
@@ -17,7 +33,7 @@ const ComingSoonPage = () => {
                 
             </div>
             <div className="jumper-img-container">
-                <ImageContainer imgPath={jumper1Img} alt="404"/>
+                <ImageContainer imgPath={images[imgIndex]} alt="404"/>
             </div>
             <div className="signup-form-and-title-container">
                 <div className="gameTitle-container">
