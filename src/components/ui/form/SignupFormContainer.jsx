ea3a15e839f460e4cd43205a0443e79aeb2021f8
Lumir-Ludvik <lumirludvik@email.cz>
2021-11-07 14:50:04 +0100

    add webpack

--- src/components/ui/form/SignupFormContainer.jsx
+++ src/components/ui/form/SignupFormContainer.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MailchimpSubscribe from "react-mailchimp-subscribe";
 import SignupForm from './SignupForm';
 
 const MailchimpFormContainer = () => {
     //TODO hide this in env
-    const postUrl = 'https://slmail.us5.list-manage.com/subscribe/post?u=76a06c328943286a2f6826939&amp;id=00fbc95a3e';
+    //const postUrl = 'https://slmail.us5.list-manage.com/subscribe/post?u=76a06c328943286a2f6826939&amp;id=00fbc95a3e';
+    const postUrl = process.env.POST_URL;
+    console.log(postUrl);
 
+    useEffect(() => console.log(postUrl),[]);
     return (
         <div className="mc__form-container">
             <MailchimpSubscribe
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const Dotenv = require('dotenv-webpack');
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './public'),
+  },
+  plugins: [
+    new Dotenv({
+        path: './.env'
+    })
+  ]
+};
