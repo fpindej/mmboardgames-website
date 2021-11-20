04d68fea47d2794d54ef1214365c566635ac9f72
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2021-11-20 23:20:42 +0100

    Add warning banner to webpage

---
+++ src/components/ui/Warning.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../styles/warning.scss';
+
+const Warning = () => {
+    return(
+        <>
+            We are working hard to release a campaign for Ski Jumping board game. We are waiting for a final administration approval from Kickstarter.<br/> 
+            Expected campaign release date is 5.–10.12.2021.<br/>
+            Please follow our page
+        </>
+    );
+}
+
+export default Warning;
