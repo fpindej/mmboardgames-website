e15e52203ae50294d80fa3d2e82c7bdfd8b74e7c
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2021-11-21 22:28:33 +0100

    Introduce smooth scrolling

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import ComingSoonPage from './components/pages/ComingSoonPage';
 
 function App() {
   return (
-    <>
-      <ComingSoonPage />
-    </>
+    <div className="app-container">
+      <section id="coming-soon">
+        <ComingSoonPage />
+      </section>
+      <section id="coming-soon-2">
+        <ComingSoonPage />
+      </section>
+    </div>
   );
 }
 
